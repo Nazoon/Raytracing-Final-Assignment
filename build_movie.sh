e142e64e710e5568d64bd687881ad7f4a18426cc
Ilan Segal <ilan.segal@mail.utoronto.ca>
2020-04-17 10:18:03 -0400

    Removed unnecessary files

--- build_movie.sh
+++ build_movie.sh
@@ -10,7 +10,6 @@ echo "Creating gif..."
 convert -delay $3 -quality $4 frames/rgb*ppm movie.gif
 date +"%c"
 echo "Converting to mp4..."
-touch movie_log.txt
-ffmpeg -i movie.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -y movie.mp4 > ./movie_log.txt
+ffmpeg -i movie.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -y -v 0 movie.mp4
 date +"%c"
 echo "Done."
